4505963bd194509d9554fad91cf909b37269b376
om-at-ust <omdubey737683r@gmail.com>
2024-10-17 15:37:41 +0530

    fixed the port

--- src/main/java/com/UST/GatewayApplication/filter/AuthenticationFilter.java
+++ src/main/java/com/UST/GatewayApplication/filter/AuthenticationFilter.java
@@ -44,7 +44,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
                     RestClient restClient = RestClient.create();
                     restClient
                             .get()
-                            .uri("http://springsecurity-sr:8082/validate/token?token=" + authHeaderToken)
+                            .uri("http://securityserver-sr:8082/validate/token?token=" + authHeaderToken)
                             .retrieve()
                             .body(Boolean.class);
                     // also instead of making a RestClient call for every request, we can validate
